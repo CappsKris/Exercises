659c7eb6af132eafd39bdf71dcd6595178dd30e3
Kristopher Capps <cappskris@yahoo.com>
2021-01-14 14:42:58 -0600

    server with a emoji

---
+++ server.js
@@ -0,0 +1,12 @@
+const emoji = require('node-emoji');
+const http = require("http");
+
+let heart = emoji.get("heart");
+console.log(heart);
+
+const server = http.createServer(function (req, res) {
+    res.write(heart);
+    res.end();
+  });
+
+server.listen(8001);
